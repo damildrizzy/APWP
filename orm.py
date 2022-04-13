b576b38cf113b8657e4253e06f723ac3aba52b4a
damildrizzy <damiloladrizzy@yahoo.com>
2022-04-13 13:50:56 +0100

    add allocation and batch table

--- orm.py
+++ orm.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Table, Integer, Column, String
-from sqlalchemy.orm import registry
+from sqlalchemy import Table, Integer, Column, String, Date, ForeignKey
+from sqlalchemy.orm import registry, relationship
 
 import model
 
@@ -15,6 +15,31 @@ order_lines = Table(
 
 )
 
+batches = Table(
+    "batches",
+    mapper_registry.metadata,
+    Column("id", Integer, primary_key=True, autoincrement=True),
+    Column("reference", String(255)),
+    Column("sku", String(255)),
+    Column("_purchased_quantity", Integer, nullable=False),
+    Column("eta", Date, nullable=True),
+)
+
+allocations = Table(
+    "allocations",
+    mapper_registry.metadata,
+    Column("id", Integer, primary_key=True, autoincrement=True),
+    Column("orderline_id", ForeignKey("order_lines.id")),
+    Column("batch_id", ForeignKey("batches.id")),
+)
+
 
 def start_mappers():
     lines_mapper = mapper_registry.map_imperatively(model.OrderLine, order_lines)
+    mapper_registry.map_imperatively(model.Batch,
+                                     batches,
+                                     properties={
+                                         "_allocations": relationship(
+                                             lines_mapper, secondary=allocations, collection_class=set,
+                                         )
+                                     })
